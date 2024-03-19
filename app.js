0624cfa928cd50cb25a029384b7bc82ebe884869
rohan <rohansharmas028@gmail.com>
2024-03-19 12:20:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var typeData = new Typed(".role" , {
+    strings: [
+    "Full Stack Web Developer",
+    "Java Programmer",
+    "Frontend Developer",
+    "Backend Developer",
+    "Coder",
+    ],
+    loop: true,
+    typeSpeed:  100,
+    backSpeed: 80,
+    backDelay: 1000,
+});
